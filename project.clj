ec8087ee5b5807f013b32bf18642d0cecc246ef5
Oliver Mooney <oliver.mooney@gmail.com>
2017-03-11 20:12:39 +0000

    Migrating resources
    - added yada as a dependency
    - created a /public directory under /resources to hold static assets
    - moved static assets from python implementation into /public
    - stood up v. minimal yada server to serve index.html when requested (need to do
      proper handler for a generic assets route, post handler to get sought query,
      etc.)

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [enlive "1.1.6"]]
+                 [enlive "1.1.6"]
+                 [yada "1.2.1"]]
   :main ^:skip-aot mickopedia.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mickopedia/core.clj
+++ src/mickopedia/core.clj
@@ -1,5 +1,17 @@
 (ns mickopedia.core
+  (:require [net.cgrand.enlive-html :as html]
+            [yada.yada :refer [listener resource as-resource]])
   (:gen-class))
+
+(def ^:dynamic *base-url* "https://wikipedia.org")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(def svr
+  (listener
+   ["/index.html" (as-resource (clojure.java.io/resource "public/index.html"))]
+   {:port 3000}))
 
 (defn -main
   "I don't do a whole lot ... yet."
