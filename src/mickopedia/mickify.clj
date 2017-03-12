a15132b8eae4521d268621424da0bf69463e462d
Oliver Mooney <oliver.mooney@gmail.com>
2017-03-12 22:22:17 +0000

    Random phrase insertion now working
    - imported phrase set and other content tweaks from python impl.
    - added text transformation function to either insert a phrase replacing a
      period, or not (33% of the time)

--- src/mickopedia/mickify.clj
+++ src/mickopedia/mickify.clj
@@ -1,16 +1,77 @@
 (ns mickopedia.mickify
-  (:require [clj-http.client :as client]
+  (:require [clojure.string :as s]
+            [clj-http.client :as client]
             [net.cgrand.enlive-html :as html]))
 
 (def ^:dynamic *wikipedia-search-url*
   "http://en.wikipedia.org/wiki/Special:Search?go=Go&search=")
+
+(def fragment-transformers
+  "2 & 3 letter transformations."
+  [])
+
+(def word-transformers
+  "Entire word transformations."
+  [])
+
+(def phrases
+  "Phrases that can be inserted to replace periods."
+  [". Bejaysus. ",". Jasus. ",". Jaysis. ",". Jaykers! ",". Sufferin'
+  Jaysus. ",". Chrisht Almighty. ",". Jesus, Mary and Joseph. ",". In fairness
+  now. ",". C'mere til I tell ya. ",". C'mere til I tell ya now. ",". C'mere til
+  I tell yiz. ",". G'wan now. ",". Arra' would ye listen to this. ",". Arra'
+  would ye listen to this shite? ",". Story? ",". Stop the lights! ",". Jesus,
+  Mary and holy Saint Joseph. ",". Whisht now. ",". Right so. ",". I hope yiz
+  are all ears now. ",". Whisht now and listen to this wan. ",". Here's a quare
+  one. ",". Here's another quare one. ",". Here's a quare one for ye. ",".
+  Here's another quare one for ye. ",". Jesus Mother of Chrisht almighty. ",".
+  Whisht now and eist liom. ",". Be the hokey here's a quare wan. ",". G'wan now
+  and listen to this wan. ",". Bejaysus here's a quare one right here now. ",".
+  Sufferin' Jaysus listen to this. ",". Holy blatherin' Joseph, listen to
+  this. ",". Listen up now to this fierce wan. ",". Would ye believe this
+  shite?",". Be the holy feck, this is a quare wan. ",". Me head is hurtin' with
+  all this raidin'. ",". Would ye swally this in a minute now?",". Bejaysus this
+  is a quare tale altogether. " ", to be sure. ",", for the craic. ",",
+  like. ",", bejaysus. ",", the hoor. ",", the cute hoor. ",", bedad. ",", Lord
+  bless us and save us. ",", fair play. ",", what? ",", game ball! ",", enda
+  story. ",". Sure this is it. ",". Soft oul' day. ",", you know yerself. ",",
+  grand so. ",", so it is. ",", you know yourself like. ",", begorrah. ",", be
+  the hokey! ",", would ye believe it? ",", would ye swally that? ",", the
+  shitehawk. ",", that's fierce now what? "])
+
+(defn random-phrase
+  "Return a random phrase from the phrases list, or a default '.', since we don't want to *always* replace a period. There's a one in three chance the period will be returned."
+  []
+  (let [phrase-count (count phrases)
+        total (+ phrase-count (quot phrase-count 2))]
+    (nth phrases (rand-int total) ".")))
+
+(def aReps
+  "Replacements for 'a'"
+  [" a bleedin' ", " an oul' ", " a feckin' ", " a holy "])
+
+(def theReps
+  "Replacements for 'the'"
+  [" the oul' ", " the bleedin' ", " the feckin' "])
+
+(defn mickify-text-node
+  "Mickify a given string."
+  [content]
+   (s/replace content "." (random-phrase)))
+
+(def mickification
+  "The mickopedia transformations."
+  (html/transformation
+   [:head] (html/prepend (html/html [:base {:href "http://en.wikipedia.org"}]))
+   [:div (html/but :a) (html/but :script) :> html/text-node] mickify-text-node
+   ))
 
 (defn transform
   "Given a collection of enlive nodes of a wikipedia page, apply the mickopedia
   transformations and return a reconstituted html page."
   [nodes]
   (-> nodes
-      (html/transform [:head] (html/prepend (html/html [:base {:href "http://en.wikipedia.org"}])))
+      mickification
       html/emit*
       (->> (apply str))))
 
