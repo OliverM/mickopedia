7055dcefb0440dface19d87b818a1fab31b216dc
Oliver Mooney <oliver.mooney@gmail.com>
2017-03-12 19:40:06 +0000

    Enlive integrated into transformation pipeline
    - just pulls apart then re-constitutes html, no changes yet

--- src/mickopedia/mickify.clj
+++ src/mickopedia/mickify.clj
@@ -2,13 +2,29 @@
   (:require [clj-http.client :as client]
             [net.cgrand.enlive-html :as html]))
 
-(def ^:dynamic *wikipedia-search-url*  "http://en.wikipedia.org/wiki/Special:Search?go=Go&search=")
+(def ^:dynamic *wikipedia-search-url*
+  "http://en.wikipedia.org/wiki/Special:Search?go=Go&search=")
 
-(defn search-wikipedia [url]
-  (-> (client/get url
-                  {:headers
-                   {"User-Agent" "Mickifier/0.2 mickopedia.org mickopedia@gmail.com"}})
-      :body))
+(defn transform
+  "Given a collection of enlive nodes of a wikipedia page, apply the mickopedia
+  transformations and return a reconstituted html page."
+  [nodes]
+  (-> nodes
+      html/emit*
+      (->> (apply str))))
+
+(defn parse-wikipedia-page
+  "Given a populated wikipedia search url, return a collection of enlive nodes
+  of wikipedia's response."
+  [url]
+  (->
+   (client/get url
+               {:headers
+                {"User-Agent"
+                 "Mickifier/0.2 mickopedia.org mickopedia@gmail.com"}})
+   :body
+   html/html-snippet
+   transform))
 
 (defn mickify [in]
-  (search-wikipedia (str *wikipedia-search-url* in)))
+  (parse-wikipedia-page (str *wikipedia-search-url* in)))
